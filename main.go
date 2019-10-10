c42bbc5e4520e79e8e8c90109993be480d4e1484
Birdy2014 <moritzv7@googlemail.com>
2019-10-10 17:56:58 +0200

    feat: faster rendering and time measurement

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	"github.com/gotk3/gotk3/cairo"
+	"bytes"
 	"github.com/gotk3/gotk3/gdk"
 	"github.com/gotk3/gotk3/gtk"
-	"math"
+	"image"
+	"image/color"
+	"image/jpeg"
 	"math/cmplx"
+	"time"
 )
 
 func fractal(c complex128, maxIter int) int {
 	z := 0 + 0i
 	i := 0
 	for ; i < maxIter && cmplx.Abs(z) < 2; i++ {
-		z = z * z + c
+		z = z*z + c
 	}
 	return i
 }
@@ -24,136 +27,194 @@ func mandelbrot(width, height, maxIter int, x1, y1, x2, y2 float64, allowDistort
 	} else {
 		xratio = yratio
 	}
-	set := make([]int, width * height)
+	set := make([]int, width*height)
 	for i := range set {
-		c := complex(x1 + xratio * float64(i%width), y1 + yratio * float64(i/width))
+		c := complex(x1+xratio*float64(i%width), y1+yratio*float64(i/width))
 		set[i] = fractal(c, maxIter)
 	}
 	return set
 }
 
-func drawMandelbrot(da *gtk.DrawingArea, cr *cairo.Context) {
+func drawMandelbrot() {
 	var set []int
 	if drawLine || (x1old == x1 && y1old == y1 && x2old == x2 && y2old == y2 && oldwidth == width && oldHeight == height && xratio == xratioold && yratio == yratioold) {
 		set = oldSet
 	} else {
-		set = mandelbrot(width, height, 300, x1, y1, x2, y2, allowDistortion)
+		start := time.Now()
+		set = mandelbrot(width, height, maxIter, x1, y1, x2, y2, allowDistortion)
+		elapsed := time.Since(start)
+		labelTime.SetText("Last mandelbrot took " + elapsed.String())
 		oldSet = set
 		x1old, y1old, x2old, y2old, xratioold, yratioold = x1, y1, x2, y2, xratio, yratio
 	}
 
-	//TODO use faster rendering
+	//TODO better colors
+	m := image.NewRGBA(image.Rect(0, 0, width, height))
+	var col color.RGBA
 	for i, c := range set {
-		if c == 300 {
-			cr.SetSourceRGB(0, 0, 0)
-		} else if c > 200 {
-			cr.SetSourceRGB(float64(c) / 400, float64(c) / 600, float64(c) / 500)
-		} else if c > 100 {
-			cr.SetSourceRGB(float64(c) / 200, float64(c) / 200, float64(c) / 300)
-		} else {
-			cr.SetSourceRGB(float64(c) / 60, float64(c) / 40, float64(c) / 40)
+		/*
+			if c == 300 {
+				col = color.RGBA{
+					R: 0,
+					G: 0,
+					B: 0,
+					A: 0,
+				}
+			} else if c > 200 {
+				col = color.RGBA{
+					R: uint8(c / 4),
+					G: uint8(c / 6),
+					B: uint8(c / 5),
+					A: 0,
+				}
+			} else if c > 100 {
+				col = color.RGBA{
+					R: uint8(c / 2),
+					G: uint8(c / 2),
+					B: uint8(c / 3),
+					A: 0,
+				}
+			} else {
+				col = color.RGBA{
+					R: uint8(c),
+					G: uint8(c * 2),
+					B: uint8(float64(c) * 1.5),
+					A: 0,
+				}
+			}
+		*/
+		col = color.RGBA{
+			R: 255 - uint8(float64(c)/float64(maxIter)*255),
+			G: 255 - uint8(float64(c)/float64(maxIter)*255),
+			B: 255 - uint8(float64(c)/float64(maxIter)*255),
+			A: 0,
 		}
 
-		cr.Rectangle(float64(i%width), float64(i/width), 1, 1)
-		cr.Fill()
+		m.Set(i%width, i/width, col)
+	}
+	col = color.RGBA{
+		R: 255,
+		G: 0,
+		B: 0,
+		A: 0,
 	}
 
 	if drawLine && secondClick {
-		cr.SetSourceRGB(1, 0, 0)
-		cr.Arc(x1pix, y1pix, 2, 0, 2 * math.Pi)
-		cr.Fill()
+		m.Set(x1pix, y1pix, col)
 	} else if drawLine {
-		cr.SetSourceRGB(1, 0, 0)
-		cr.MoveTo(x1pix, y1pix)
-		cr.LineTo(x2pix, y1pix)
-		cr.LineTo(x2pix, y2pix)
-		cr.LineTo(x1pix, y2pix)
-		cr.LineTo(x1pix, y1pix)
-		cr.Stroke()
+		rect(m, x1pix, y1pix, x2pix, y2pix, col)
 		drawLine = false
+	}
+
+	buf := new(bytes.Buffer)
+	jpeg.Encode(buf, m, nil)
+	pixbufLoader, _ := gdk.PixbufLoaderNew()
+	pixbufLoader.Write(buf.Bytes())
+	pixbuf, _ := pixbufLoader.GetPixbuf()
+	pixbufLoader.Close()
+	img.SetFromPixbuf(pixbuf)
+}
+
+func rect(img *image.RGBA, x1, y1, x2, y2 int, col color.Color) {
+	for i := x1; i < x2; i++ {
+		img.Set(i, y1, col)
+		img.Set(i, y2, col)
+	}
+	for i := y1; i < y2; i++ {
+		img.Set(x1, i, col)
+		img.Set(x2, i, col)
 	}
 }
 
-var width, height int											//Size of the drawingarea, updated when resized
-var x1, y1, x2, y2 float64 = -2, -1.25, 0.5, 1.25   			//mandelbrot coordinates to render on next draw
-var x1old, y1old, x2old, y2old float64							//old coordinates and size so it doesn't have to re-render everything if nothing changes
+var width, height int                             //Size of the drawingarea, updated when resized
+var x1, y1, x2, y2 float64 = -2, -1.25, 0.5, 1.25 //mandelbrot coordinates to render on next draw
+var x1old, y1old, x2old, y2old float64            //old coordinates and size so it doesn't have to re-render everything if nothing changes
 var oldwidth, oldHeight int
-var xratio, yratio, xratioold, yratioold, x1new, y1new float64            			//ratio pixel - coordinates and the new top left coordinates that are set on the first click
+var xratio, yratio, xratioold, yratioold, x1new, y1new float64 //ratio pixel - coordinates and the new top left coordinates that are set on the first click
 var secondClick, drawLine bool
-var x1pix, y1pix, x2pix, y2pix float64							//clicked position in pixels on screen for drawing lines
+var x1pix, y1pix, x2pix, y2pix int //clicked position in pixels on screen for drawing lines
 var oldSet []int
 var allowDistortion bool = false
+var maxIter int = 500
+var img *gtk.Image
+var labelTime *gtk.Label
+
 func main() {
 	gtk.Init(nil)
-	win, _ := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
 
+	//// Create Window ////
+
+	win, _ := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
 	win.SetTitle("Mandelbrot")
+	win.SetDefaultSize(800, 600)
 	win.Connect("destroy", func() {
 		gtk.MainQuit()
 	})
 
+	//// Create Widgets ////
+
 	box, _ := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 1)
 	buttonbox, _ := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 1)
 
-	b, _ := gtk.ButtonNewWithLabel("Generate Mandelbrot")
-	b.Connect("clicked", win.QueueDraw)
-
+	buttonStart, _ := gtk.ButtonNewWithLabel("Generate Mandelbrot")
 	buttonReset, _ := gtk.ButtonNewWithLabel("Reset")
+	labelTime, _ = gtk.LabelNew("")
+
+	eventBox, _ := gtk.EventBoxNew()
+	img, _ = gtk.ImageNew()
+
+	//// Signals ////
+
+	buttonStart.Connect("clicked", drawMandelbrot)
+
 	buttonReset.Connect("clicked", func() {
 		x1, y1, x2, y2 = -2, -1.25, 0.5, 1.25
-		win.QueueDraw()
+		drawMandelbrot()
 	})
 
-	buttonResetAspect, _ := gtk.ButtonNewWithLabel("Reset aspect ratio")
-	buttonResetAspect.Connect("clicked", func() {
-		yratio = xratio
-		win.QueueDraw()
-	})
-
-	da, _ := gtk.DrawingAreaNew()
-	da.AddEvents(gdk.GDK_BUTTON1_MASK)
-	da.Connect("size-allocate", func(da *gtk.DrawingArea) {
+	eventBox.Connect("size-allocate", func(eventBox *gtk.EventBox) {
 		oldwidth = width
 		oldHeight = height
-		allocation := da.GetAllocation()
+		allocation := img.GetAllocation()
 		width = allocation.GetWidth()
 		height = allocation.GetHeight()
 	})
-	da.Connect("button-press-event", func(da *gtk.DrawingArea, event *gdk.Event) {
+
+	eventBox.Connect("button-press-event", func(eventBox *gtk.EventBox, event *gdk.Event) {
 		secondClick = !secondClick
 		eventButton := gdk.EventButtonNewFromEvent(event)
 		if secondClick {
-			x1new = x1 + xratio * eventButton.X()
-			y1new = y1 + yratio * eventButton.Y()
+			x1new = x1 + xratio*eventButton.X()
+			y1new = y1 + yratio*eventButton.Y()
 
-			x1pix = eventButton.X()
-			y1pix = eventButton.Y()
+			x1pix = int(eventButton.X())
+			y1pix = int(eventButton.Y())
 
 			drawLine = true
-			win.QueueDraw()
+			drawMandelbrot()
 		} else {
-			x2 = x1 + xratio * eventButton.X()
-			y2 = y1 + yratio * eventButton.Y()
+			x2 = x1 + xratio*eventButton.X()
+			y2 = y1 + yratio*eventButton.Y()
 			x1 = x1new
 			y1 = y1new
 
-			x2pix = eventButton.X()
-			y2pix = eventButton.Y()
+			x2pix = int(eventButton.X())
+			y2pix = int(eventButton.Y())
 
 			drawLine = true
-			win.QueueDraw()
+			drawMandelbrot()
 		}
 	})
-	da.Connect("draw", drawMandelbrot)
 
-	box.PackStart(da, true, true, 0)
-	buttonbox.Add(b)
+	//// Add Widgets to Window ////
+
+	eventBox.Add(img)
+	box.PackStart(eventBox, true, true, 0)
+	buttonbox.Add(buttonStart)
 	buttonbox.Add(buttonReset)
-	buttonbox.Add(buttonResetAspect)
+	buttonbox.Add(labelTime)
 	box.Add(buttonbox)
 	win.Add(box)
-
-	win.SetDefaultSize(800, 600)
 
 	win.ShowAll()
 
